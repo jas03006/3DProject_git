31f9654493ef08b177d47fc86936096f77de7322
laurel3394 <b4comes@gmail.com>
2023-10-27 14:40:29 +0900

    [test] SkyBox 회전추가
    skybox 에셋 4개 추가

---
+++ Flappy_Bird/Assets/3.Script/Pooling/BackGround/Test/SkyBoxRot.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxRot : MonoBehaviour //하늘 움직이는거, 이거 걍 게임매니저에 박아도 될듯???
+{
+    [SerializeField] private float SkySpeed = 0f;
+    private void Update()
+    {
+        SkySpeedMove();
+    }
+    private void SkySpeedMove()
+    {
+        SkySpeed += Time.deltaTime*2;
+        if (SkySpeed >= 360)
+        {
+            SkySpeed = 0f;
+        }
+        RenderSettings.skybox.SetFloat("_Rotation", SkySpeed);
+    }
+}
