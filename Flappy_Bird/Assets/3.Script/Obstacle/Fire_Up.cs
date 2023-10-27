f9ac455504b4f0959919569f6e46e48dcfe4839c
jas03006 <jas03006@kaist.ac.kr>
2023-10-27 21:05:14 +0900

    [Feat] 효과음 볼륨 조정

--- Flappy_Bird/Assets/3.Script/Obstacle/Fire_Up.cs
+++ Flappy_Bird/Assets/3.Script/Obstacle/Fire_Up.cs
@@ -6,23 +6,24 @@ public class Fire_Up : MonoBehaviour
 {
     private ParticleSystem ps;
     private BoxCollider collider;
+    private AudioSource audio_source;
     private void Start()
     {
         TryGetComponent(out ps);
         TryGetComponent(out collider);
-        
+        TryGetComponent(out audio_source);
     }
 
     private void Update()
-    {
-        
+    {        
         if (ps.particleCount >0 )
         {
             collider.enabled = true;
-           // Debug.Log(ps.particleCount);
+            //audio_source.Play();
         }
         else {
             collider.enabled = false;
+            //audio_source.Stop();
         }
     }
 
